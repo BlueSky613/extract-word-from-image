9ef5562543c8ed1e187acc9be3b14abdcf99d1e8
Blue Sky <stylistdire68@gmail.com>
2025-03-04 07:18:38 -0500

    Change coordintaes based on right bottom

--- extract.py
+++ extract.py
@@ -5,22 +5,29 @@ import csv
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\kk\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 image_path = 'Lorem2.png'
 image = cv2.imread(image_path)
+img_height, img_width, channels = image.shape
 data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
 target_sentence = 'Adsfadstdsat'
+words = target_sentence.split()
 coordinates = []
 for i in range(len(data['text'])):
-     if data['text'][i].lower() == target_sentence.lower():
-        x = data['left'][i]
-        y = data['top'][i]
-        width = data['width'][i]
-        height = data['height'][i]
-        coordinates.append((x, y, width, height))
+     if data['text'][i].lower() == words[0].lower():
+        if data['text'][i + len(words) - 1].lower() == words[len(words) - 1].lower():
+            x1 = data['left'][i]
+            y1 = data['top'][i]
+            width1 = data['width'][i]
+            height1 = data['height'][i]
+            x2 = data['left'][i + len(words) - 1]
+            y2 = data['top'][i + len(words) - 1]
+            width2 = data['width'][i + len(words) - 1]
+            height2 = data['height'][i + len(words) - 1]
+            coordinates.append((x1, y1, width1, height1, x2, y2, width2, height2))
 csv_file_path = "Lorem2.csv"
 if coordinates:
     for coord in coordinates:
         with open(csv_file_path, mode='a', newline='', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow([target_sentence, coord[0], coord[1]])
+            writer.writerow([target_sentence, (coord[0] + coord[4] + coord[6])/2, img_height-(coord[1] + coord[3]/2)])
         print(f"Coordinates of the sentence '{target_sentence}':")
         print(f"X={coord[0]}, Y={coord[1]}")
 
